3ba17120822ee651038f5757987b0d98ee3cb57d
dnnnah <dnnnah@icloud.com>
2026-04-21 22:10:28 -0600

    fix: force seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 php artisan migrate --force
-php artisan db:seed --class=MoviesTableSeeder --force || true
+php artisan db:seed --class=MoviesTableSeeder --force
 php artisan serve --host=0.0.0.0 --port=8080
